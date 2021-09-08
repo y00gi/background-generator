658ca4adf11debff3e1602f1858aa4e04edb990a
y00gi <yogendrajput0@gmail.com>
2021-09-08 16:22:51 +0530

    added frame

---
+++ script.js
@@ -0,0 +1,12 @@
+const hThree = document.querySelector('h3');
+const colorOne = document.querySelector('.color1');
+const colorTwo = document.querySelector('.color2');
+const body = document.getElementsByTagName('body');
+
+colorOne.addEventListener('input', function(){
+    
+})
+
+colorTwo.addEventListener('input', function(){
+    
+})
